37abad9fe8887683a7f642338cc88edcae24fe21
Connor <concon477@virginmedia.com>
2024-01-23 18:37:19 +0000

    feat(Core): More user friendly messages for event trace debugging

--- Core/src/main/java/com/readutf/inari/core/commands/EventDebugCommand.java
+++ Core/src/main/java/com/readutf/inari/core/commands/EventDebugCommand.java
@@ -3,9 +3,11 @@ package com.readutf.inari.core.commands;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import com.readutf.inari.core.event.GameEventManager;
+import com.readutf.inari.core.utils.ColorUtils;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
 
 @RequiredArgsConstructor
 public class EventDebugCommand extends BaseCommand {
@@ -14,13 +16,34 @@ public class EventDebugCommand extends BaseCommand {
 
     @CommandAlias("eventdebug")
     public void onEventDebug(Player player, String event) {
-        try {
-            Class<?> aClass = getClass().getClassLoader().loadClass(event);
 
-            eventManager.getActiveDebugs().add((Class<? extends Event>) aClass);
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+        Class<? extends Event> eventClass = findEventClass(event);
+        if (eventClass == null) {
+            player.sendMessage(ColorUtils.color("&cEvent not found, make sure to copy the exact class reference"));
+            return;
         }
+
+        if (eventManager.getActiveDebugs().remove(eventClass)) {
+            player.sendMessage(ColorUtils.color("&aDisabled debug for " + eventClass.getSimpleName()));
+            return;
+        } else {
+            eventManager.getActiveDebugs().add(eventClass);
+            player.sendMessage(ColorUtils.color("&aEnabled debug for " + eventClass.getSimpleName()));
+            return;
+        }
+
     }
+
+    public @Nullable Class<? extends Event> findEventClass(String reference) {
+
+        try {
+            Class<?> aClass = getClass().getClassLoader().loadClass(reference);
+
+            return aClass.asSubclass(Event.class);
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
 }
